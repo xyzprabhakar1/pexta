2582b238058fce1cefdce8dea9ea08a04ada0417
PRABHAKAR KUMAR SINGH <prabhakarks90@hotmail.com>
2022-06-25 18:35:47 +0530

    OK

--- Common/LinqHelper.cs
+++ Common/LinqHelper.cs
@@ -28,77 +28,49 @@ namespace Common
             return query;
         }
 
-        public static IQueryable<T> CurrentData<T>(IQueryable<T> source,string ColumnName= "EffectiveDt")
-        {
+        public static IQueryable<T> CurrentData<T>(IQueryable<T> source,string GroupColumnName= "EmpId", string DateColumnName= "EffectiveDt") where T : class
+        {   
             DateTime CurrentDate=DateTime.Now;
-            var classType = typeof(T);
-            PropertyInfo? pi_EffectiveDt = classType.GetProperty(ColumnName);            
-            if (pi_EffectiveDt == null)
+            Type classType = typeof(T);
+            PropertyInfo? pi_EffectiveDt_ = classType.GetProperty(DateColumnName);
+            PropertyInfo? pi_GroupBy_ = classType.GetProperty(GroupColumnName);
+            if (pi_EffectiveDt_ == null)
             {
-                return source;
+                throw new Exception("Invalid column Name");
             }
+            if (pi_GroupBy_ == null)
+            {
+                throw new Exception("Invalid column Name");
+            }
+            PropertyInfo pi_EffectiveDt = pi_EffectiveDt_;
+            PropertyInfo pi_GroupBy = pi_GroupBy_;
+
             PropertyInfo? pi_IsActive = classType.GetProperty("IsActive");
             PropertyInfo? pi_IsDeleted= classType.GetProperty("IsDeleted");
             ParameterExpression param = Expression.Parameter(classType, "t");
-            var EffectiveDt_ = Expression.Property(param, "EffectiveDt");
+
+            MemberExpression EffectiveDt_ = Expression.Property(param, DateColumnName);
+            MemberExpression GroupBy_ = Expression.Property(param, GroupColumnName);
+            
             var CurrentDt_ = Expression.Constant(CurrentDate, typeof(DateTime));
+            var tempbool_ = Expression.Constant(true, typeof(bool));
             Expression.LessThan(EffectiveDt_, CurrentDt_);
             MemberExpression? IsActive_ = pi_IsActive == null ? null : Expression.Property(param, "IsActive");
             MemberExpression? IsDeleted_ = pi_IsDeleted == null ? null : Expression.Property(param, "IsDeleted");
-            LambdaExpression FirstCondition = (IsActive_ != null && IsDeleted_ != null) ? FirstCondition_EffectiveDate_IsActive_IsDeleted():
-                (IsActive_ != null && IsDeleted_ == null)? FirstCondition_EffectiveDate_IsActive():
-                (IsActive_ == null && IsDeleted_ != null) ? FirstCondition_EffectiveDate_IsDeleted() :
-                FirstCondition_EffectiveDate_IsActive()
-                ;
+
+            var Condition1 =
+            ((IsActive_ != null && IsDeleted_ != null) ? Expression.Lambda<Func<T, bool>>(Expression.AndAlso(Expression.AndAlso(Expression.LessThan(EffectiveDt_, CurrentDt_),Expression.Not(IsDeleted_)), IsActive_), new ParameterExpression[] { param }):
+            (IsActive_ != null && IsDeleted_ == null)? Expression.Lambda<Func<T, bool>>(Expression.AndAlso(Expression.LessThan(EffectiveDt_, CurrentDt_),IsActive_), new ParameterExpression[] { param }) :
+            (IsActive_ == null && IsDeleted_ != null) ? Expression.Lambda<Func<T, bool>>(Expression.AndAlso(Expression.LessThan(EffectiveDt_, CurrentDt_),Expression.Not(IsDeleted_)), new ParameterExpression[] { param }) :
+            Expression.Lambda<Func<T, bool>>(Expression.LessThan(EffectiveDt_, CurrentDt_), new ParameterExpression[] { param }));
             
+            var Condition2 = Expression.Lambda<T,Tkey>(GroupBy_);
+            IQueryable <T> query = source.Where(Condition1).GroupBy(Condition2) ;
+
             
 
-            LambdaExpression FirstCondition_EffectiveDate()
-            { 
-                return Expression.Lambda(
-                Expression.LessThan(EffectiveDt_, CurrentDt_));
-            }
-            LambdaExpression FirstCondition_EffectiveDate_IsActive()
-            {   
-                return Expression.Lambda(
-                    Expression.AndAlso(
-                        Expression.LessThan(EffectiveDt_, CurrentDt_),
-                        IsActive_)
-                );
-            }
-            LambdaExpression FirstCondition_EffectiveDate_IsDeleted()
-            {
-                return Expression.Lambda(
-                    Expression.AndAlso(
-                        Expression.LessThan(EffectiveDt_, CurrentDt_),
-                        Expression.Not( IsDeleted_))
-                );
-            }
-            LambdaExpression FirstCondition_EffectiveDate_IsActive_IsDeleted()
-            {
-                return Expression.Lambda(
-                    Expression.AndAlso(
-                    Expression.AndAlso(
-                        Expression.LessThan(EffectiveDt_, CurrentDt_),
-                        Expression.Not(IsDeleted_)),IsActive_
-                    )
-                );
-            }
 
-            string sortingDir = string.Empty;
-            if (sortDirection.ToUpper().Trim() == "ASC")
-                sortingDir = "Where";
-            else if (sortDirection.ToUpper().Trim() == "DESC")
-                sortingDir = "OrderByDescending";
             
-            
-            PropertyInfo pi = typeof(T).GetProperty(sortExpression);
-            pi.Name
-            Type[] types = new Type[2];
-            types[0] = typeof(T);
-            types[1] = pi.PropertyType;
-            Expression expr = Expression.Call(typeof(Queryable), sortingDir, types, source.Expression, Expression.Lambda(Expression.Property(param, sortExpression), param));
-            IQueryable<T> query = source.AsQueryable().Provider.CreateQuery<T>(expr);
             return query;
         }
     }
--- HRMS/Controllers/EmployeeController.cs
+++ HRMS/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using HRMS.classes.repository;
+using HRMS.Database;
 using HRMS.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,20 +13,28 @@ namespace HRMS.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IserEmployee _serEmployee;
-        public EmployeeController(IserEmployee serEmployee)
+        HRMSContext _hRMSContext;
+        public EmployeeController(IserEmployee serEmployee, HRMSContext hRMSContext)
         {
             _serEmployee = serEmployee;
+            _hRMSContext= hRMSContext;
         }
         [HttpPost]
         [Route("EmpBasicDetails")]
-        public IEnumerable<mdlEmployeeBasic> EmpBasicDetails(DataTableParameters dtp=null)
+        public IEnumerable<mdlEmployeeBasic> EmpBasicDetails(DataTableParameters dtp = null)
         {
             DateTime CurrentDt = DateTime.Now;
             Dictionary<uint, string> Departmentlst = new Dictionary<uint, string>();
             Dictionary<uint, string> Locationlst = new Dictionary<uint, string>();
             return _serEmployee.GetBasicDetail(CurrentDt, false, Departmentlst, Locationlst, true);
-            var tempData= _serEmployee.GetBasicDetail(CurrentDt, false, Departmentlst, Locationlst, true).ToList();
+            var tempData = _serEmployee.GetBasicDetail(CurrentDt, false, Departmentlst, Locationlst, true).ToList();
             return tempData;
+        }
+        [HttpPost]
+        [Route("test")]
+        public IEnumerable<tblEmpLocation> test()
+        {
+            return LinqHelper.CurrentData(_hRMSContext.tblEmpLocation, "EffectiveDt");
         }
 
     }
